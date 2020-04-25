5ab7bbc30c282ece2c47f40da8da6ee37aa63c8f
Biruk Adera <aderabiruk@gmail.com>
2020-04-25 22:36:33 +0300

    errors Related Files Prepared for Auth Package

---
+++ packages/auth/src/errors/constants.ts
@@ -0,0 +1,30 @@
+export const ERROR_STATUS_CODES = {
+    BAD_INPUT_ERROR: 400,
+    UNAUTHORIZED_ERROR: 401,
+    FORBIDDEN_ERROR: 403,
+    NOT_FOUND_ERROR: 404,
+    INTERNAL_SERVER_ERROR: 500
+};
+
+export const ERROR_MESSAGES = {
+    /**
+     * Authenticated Related Error Messages
+     */
+    AUTHENTICATION_ERROR: "Login Failed: Invalid email or password!",
+    FORBIDDEN_ERROR: "Authorization Failure: You're not allowed!",
+
+    /**
+     * Client Error
+     */
+    CLIENT_NOT_FOUND_ERROR: "Client not found!",
+
+    /**
+     * General Error
+     */
+    INTERNAL_SERVER_ERROR: "Internal Error: Something went wrong!",
+
+    /**
+     * User Error
+     */
+    USER_NOT_FOUND_ERROR: "User not found!",
+};
