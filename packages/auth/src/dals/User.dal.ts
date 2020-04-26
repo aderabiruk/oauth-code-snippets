977ea0e00b926f8007b438d10bf78574bd43e608
Biruk Adera <aderabiruk@gmail.com>
2020-04-26 19:08:04 +0300

    User DAL Preared

---
+++ packages/auth/src/dals/User.dal.ts
@@ -0,0 +1,64 @@
+import User from "../models/User";
+
+class UserDAL {
+
+    /**
+     * Create User
+     * 
+     * @param {String} email     User email address
+     * @param {String} password  User password
+     */
+    static create(email: String, password: String): Promise<any> {
+        return new Promise((resolve, reject) => {
+            let user = new User();
+            user.email = email;
+            user.password = password;
+            user.save((error, savedUser) => {
+                if (error) {
+                    reject(error);
+                }
+                else {
+                    resolve(savedUser);
+                }
+            });
+        });
+    }
+
+    /**
+     * Find Many Users
+     * 
+     * @param {any} query Query Object
+     */
+    static findMany(query: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            User.find({...query, deleted_at: null}, (error, users) => {
+                if (error) {
+                    reject(error);
+                }
+                else {
+                    resolve(users);
+                }
+            });
+        });
+    }
+
+    /**
+     * Find a User
+     * 
+     * @param {any} query Query Object
+     */
+    static findOne(query: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            User.findOne({...query, deleted_at: null}, (error, users) => {
+                if (error) {
+                    reject(error);
+                }
+                else {
+                    resolve(users);
+                }
+            });
+        });
+    }
+}
+
+export default UserDAL;
