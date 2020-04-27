105a81fdc13b371f81ae9d9617e306b2a56eebe5
Biruk Adera <aderabiruk@gmail.com>
2020-04-27 23:04:28 +0300

    Token Endpoint Service Prepared

--- packages/auth/src/services/Auth.service.ts
+++ packages/auth/src/services/Auth.service.ts
@@ -1,20 +1,24 @@
 import _ from 'lodash';
 import async from 'async';
+import cryptoRandomString from 'crypto-random-string';
 
 import { Client } from '../models/Client';
 import ClientDAL from '../dals/Client.dal';
 import { ERROR_MESSAGES } from '../errors/constants';
-import { NotFoundError, BadInputError } from '../errors/errors';
+import { AuthorizationCode } from '../models/AuthorizationCode';
+import AuthorizationCodeDAL from '../dals/AuthorizationCode.dal';
+import { generateAccessTokenForClient } from '../utils/security';
+import { NotFoundError, BadInputError, InternalServerError, UnauhtorizedError } from '../errors/errors';
 
 class AuthService {
 
     /**
      * Client Authorization
      * 
-     * @param response_type OAuth Response Type
-     * @param client_id     Client ID
-     * @param redirect_uri  Redirect URI
-     * @param scopes        Scopes
+     * @param {String} response_type OAuth Response Type
+     * @param {String} client_id     Client ID
+     * @param {String} redirect_uri  Redirect URI
+     * @param {String} scopes        Scopes
      */
     static authorize(response_type: string, client_id: string, redirect_uri: string, scopes: string): Promise<any> {
         return new Promise((resolve, reject) => {
@@ -52,6 +56,7 @@ class AuthService {
                     }
                     else {
                         resolve({
+                            client: client,
                             isAuthorized: true
                         });
                     }
@@ -64,15 +69,127 @@ class AuthService {
         });
     }
 
-    static approve(): Promise<any> {
+    /**
+     * Client Approved
+     * 
+     * @param {String} client_id     Client ID
+     * @param {String} redirect_uri  Redirect URI
+     * @param {String} scopes        Scopes 
+     */
+    static approve(client_id: string, redirect_uri: string, scopes: string): Promise<any> {
         return new Promise((resolve, reject) => {
-
+            async.waterfall([
+                (done: Function) => {
+                    ClientDAL.findOne({ _id: client_id })
+                        .then((client: Client) => {
+                            if (client) {
+                                done(null, client);
+                            }
+                            else {
+                                done(new NotFoundError(ERROR_MESSAGES.CLIENT_NOT_FOUND_ERROR));
+                            }
+                        })
+                        .catch((error: any) => {
+                            done(new NotFoundError(error));
+                        });
+                },
+                (client: Client, done: Function) => {
+                    if (client.redirect_uri !== redirect_uri) {
+                        done(new BadInputError([
+                            { field: "redirect_uri", message: ERROR_MESSAGES.CLIENT_REDIRECT_URI_INVALID_ERROR }
+                        ]));
+                    }
+                    else {
+                        done(null, client);
+                    }
+                },
+                (client: Client, done: Function) => {
+                    let rscope = scopes ? scopes.split(" ") : [];
+                    if (_.difference(rscope, client.scopes).length > 0) {
+                        done(new BadInputError([
+                            { field: "redirect_uri", message: ERROR_MESSAGES.CLIENT_SCOPE_INVALID_ERROR }
+                        ]));
+                    }
+                    else {
+                        let code = cryptoRandomString({ length: 10 });
+                        AuthorizationCodeDAL.create(client._id, code, scopes)
+                            .then((authroizationCode: AuthorizationCode) => {
+                                resolve(authroizationCode);  
+                            })
+                            .catch((error: any) => {
+                                done(new BadInputError(error));
+                            })
+                    }
+                }
+            ], (error) => {
+                if (error) {
+                    reject(error);
+                }
+            });
         });
     }
 
-    static token(): Promise<any> {
+    /**
+     * Generate Access Token
+     * 
+     * @param {string} client_id        Client ID
+     * @param {string} client_secret    Client Secret
+     * @param {string} code             Authorization Code
+     * @param {string} redirect_uri     Redirect URI
+     */
+    static token(client_id: string, client_secret: string, code: string, redirect_uri: string): Promise<any> {
         return new Promise((resolve, reject) => {
-
+            async.waterfall([
+                (done: Function) => {
+                    ClientDAL.findOne({ _id: client_id })
+                        .then((client: Client) => {
+                            if (client) {
+                                done(null, client);
+                            }
+                            else {
+                                done(new NotFoundError(ERROR_MESSAGES.CLIENT_NOT_FOUND_ERROR));
+                            }
+                        })
+                        .catch((error: any) => {
+                            done(new NotFoundError(error));
+                        });
+                },
+                (client: Client, done: Function) => {
+                    if (client.secret === client_secret) {
+                        done(null, client);
+                    }
+                    else {
+                        done(new NotFoundError(ERROR_MESSAGES.CLIENT_NOT_FOUND_ERROR));
+                    }
+                },
+                (client: Client, done: Function) => {
+                    AuthorizationCodeDAL.findOne({ code: code })
+                        .then((authorizationCode: AuthorizationCode) => {
+                            if (authorizationCode) {
+                                done(null, client, authorizationCode);
+                            }
+                            else {
+                                done(new UnauhtorizedError());
+                            }
+                        })
+                        .catch((error: any) => {
+                            done(new InternalServerError(error));
+                        });
+                },
+                (client: Client, authorizationCode: AuthorizationCode, done: Function) => {
+                    resolve({
+                        access_token: generateAccessTokenForClient(client, authorizationCode.scope, "30d"),
+                        token_type: 'Bearer',
+                        expires_in: 60 * 60 * 24 * 30,
+                        refresh_token: '',
+                        scope: authorizationCode.scope
+                    });
+                }
+            ], (error) => {
+                if (error) {
+                    reject(error);
+                }
+            });
         });
     }
 };
